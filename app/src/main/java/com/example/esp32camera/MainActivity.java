1562cfe33533c61e819aaea629388a908c69909a
Jens Behmenburg <jensbehmenburg@gmail.com>
2021-11-12 19:21:34 +0100

    feat: added WebSocketClient & Constants

--- app/src/main/java/com/example/esp32camera/MainActivity.java
+++ app/src/main/java/com/example/esp32camera/MainActivity.java
@@ -1,14 +1,107 @@
 package com.example.esp32camera;
 
+import static com.example.esp32camera.util.Constants.STREAM_PATH;
+import static com.example.esp32camera.util.Constants.WEBSERVER_URL;
+import static com.example.esp32camera.util.Constants.WEBSOCKETS_SERVER_URL;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import org.java_websocket.client.WebSocketClient;
+import org.java_websocket.handshake.ServerHandshake;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
+
+    private WebSocketClient webSocketClient;
+    private WebView webview;
+    private EditText et_webSocketInput;
+    private Button button_send;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        webview = findViewById(R.id.webViewStream);
+        et_webSocketInput = findViewById(R.id.et_webSocketInput);
+        button_send = findViewById(R.id.button_send);
+
+
+        setupCameraStream();
+
+        connectWebSocket();
+
+        button_send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!et_webSocketInput.getText().equals("")) {
+                    String message = et_webSocketInput.getText().toString();
+                    webSocketClient.send(message);
+                    et_webSocketInput.setText("");
+                }
+            }
+        });
+    }
+
+    private void setupCameraStream() {
+        webview.getSettings().setLoadWithOverviewMode(true);
+        webview.getSettings().setUseWideViewPort(true);
+        // because desktop mode is needed:
+        // https://stackoverflow.com/questions/67849381/esp32-httpd-header-fields-are-too-long-for-the-server-to-interpret
+        String newUserAgent = "Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.4) Gecko/20100101 Firefox/4.0";
+        webview.getSettings().setUserAgentString(newUserAgent);
+        webview.loadUrl(WEBSERVER_URL + STREAM_PATH); // start webView
+    }
+
+    private void connectWebSocket() {
+
+        URI uri = null;
+        try {
+            uri = new URI(WEBSOCKETS_SERVER_URL);
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+
+
+        webSocketClient = new WebSocketClient(Objects.requireNonNull(uri)) {
+            @Override
+            public void onOpen(ServerHandshake serverHandshake) {
+                Log.i("Websocket", "Opened");
+                webSocketClient.send("Hello from " + Build.MANUFACTURER + " " + Build.MODEL);
+            }
+
+            @Override
+            public void onMessage(String s) {
+                final String message = s;
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(MainActivity.this, message, Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+
+            @Override
+            public void onClose(int i, String s, boolean b) {
+                Log.i("Websocket", "Closed " + s);
+            }
+
+            @Override
+            public void onError(Exception e) {
+                Log.i("Websocket", "Error " + e.getMessage());
+            }
+        };
+        webSocketClient.connect();
     }
 }
---
+++ app/src/main/java/com/example/esp32camera/util/Constants.java
@@ -0,0 +1,11 @@
+package com.example.esp32camera.util;
+
+public class Constants {
+    // Communication
+    public static final String WEBSERVER_URL = "http://192.168.188.45:80";
+    public static final String WEBSOCKETS_SERVER_URL = "ws://192.168.188.45:81";
+
+    // Paths
+    public static final String STREAM_PATH = "/stream";
+
+}
