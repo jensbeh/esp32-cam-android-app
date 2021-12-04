3e995a31c91edd338795c797feeb5b31c18ba2b0
Jens Behmenburg <jensbehmenburg@gmail.com>
2021-12-04 19:36:38 +0100

    feat: added deterministic loading circle for searching espCameras

--- app/src/main/java/com/esp32camera/bottomSheets/BottomSheetAddEspCamera.java
+++ app/src/main/java/com/esp32camera/bottomSheets/BottomSheetAddEspCamera.java
@@ -2,6 +2,8 @@ package com.esp32camera.bottomSheets;
 
 import static android.content.Context.WIFI_SERVICE;
 
+import static java.lang.Math.round;
+
 import android.content.Context;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
@@ -9,11 +11,16 @@ import android.os.AsyncTask;
 import android.text.format.Formatter;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -30,9 +37,11 @@ import java.util.List;
 public class BottomSheetAddEspCamera extends BottomSheetDialog {
     private final Context context;
     private final View bottomSheetView;
+    private final ProgressBar progressBarLoading;
+    private final TextView tv_searching;
     private MainPresenter mainPresenter;
     private RecyclerView rv_add_espCameras;
-    private LinearLayout loadingLayout;
+    private ConstraintLayout loadingLayout;
 
     private List<String> espCamerasIp;
 
@@ -46,8 +55,11 @@ public class BottomSheetAddEspCamera extends BottomSheetDialog {
 
         espCamerasIp = new ArrayList<>();
 
-        loadingLayout = (LinearLayout) bottomSheetView.findViewById(R.id.loadingLayout);
+        loadingLayout = (ConstraintLayout) bottomSheetView.findViewById(R.id.loadingLayout);
         loadingLayout.setVisibility(View.VISIBLE);
+        progressBarLoading = (ProgressBar) bottomSheetView.findViewById(R.id.progressBarLoading);
+        progressBarLoading.setProgress(0);
+        tv_searching = (TextView) bottomSheetView.findViewById(R.id.tv_searching);
 
         loadAllEspCameras(new ReadyCallback() {
             @Override
@@ -72,6 +84,13 @@ public class BottomSheetAddEspCamera extends BottomSheetDialog {
      * @param readyCallback to setup RV of cameras and set loadingScreen to Gone
      */
     private void loadAllEspCameras(ReadyCallback readyCallback) {
+        Animation anim = new AlphaAnimation(0.3f, 1.0f);
+        anim.setDuration(500); //You can manage the blinking time with this parameter
+        anim.setStartOffset(100);
+        anim.setRepeatMode(Animation.REVERSE);
+        anim.setRepeatCount(Animation.INFINITE);
+        tv_searching.startAnimation(anim);
+
         AsyncTask.execute(() -> {
             WifiManager wifiMgr = (WifiManager) mainPresenter.getActivity().getApplicationContext().getSystemService(WIFI_SERVICE);
             WifiInfo wifiInfo = wifiMgr.getConnectionInfo();
@@ -80,13 +99,14 @@ public class BottomSheetAddEspCamera extends BottomSheetDialog {
 
             String currentSubIpAddress = ipAddress.substring(0, ipAddress.lastIndexOf("."));
             int timeout = 100;
-            for (int i = 1; i < 255; i++) {
+            int ipAddressMaxCount = 255;
+            for (int i = 1; i < ipAddressMaxCount; i++) {
                 String host = currentSubIpAddress + "." + i;
                 try {
                     if (InetAddress.getByName(host).isReachable(timeout)) {
                         InetAddress addr = InetAddress.getByName(host);
                         String hostName = addr.getHostName();
-                        if (hostName.contains("ESP-Camera")) {
+                        if (hostName.contains("ESP-Camera") && !mainPresenter.ifCameraExisting(host)) {
                             System.out.println(host + "isreachable");
                             System.out.println(hostName);
                             espCamerasIp.add(host);
@@ -96,6 +116,7 @@ public class BottomSheetAddEspCamera extends BottomSheetDialog {
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
+                progressBarLoading.setProgress((int)(round(i * (100.0 / ipAddressMaxCount))));
             }
             readyCallback.onReady();
         });
